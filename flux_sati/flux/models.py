dcf65f2811e6216b70880336d1d1e936dc561344
Jatin-tec <jatin.kshatriya2821@gmail.com>
2021-12-23 18:43:50 +0530

    image slider

--- flux_sati/flux/models.py
+++ flux_sati/flux/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False, verbose_name="Project Name")
+    description = models.CharField(max_length=1000, blank=True, null=True, verbose_name="project description")
+    image = models.ImageField(upload_to='media', blank=True, null=True, default='')
+    completed = models.BooleanField(null=False, blank=False, default=False, verbose_name="Is this project compleated")
+    team = models.TextField(null=True, blank=True, default='[]', verbose_name='Team members')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Projects" 
+
+class Events(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False, verbose_name="Event Name")           
+    organizer = models.CharField(max_length=50, blank=False, null=False, default='CLUB FLUX', verbose_name="Event Organizer")
+    team = models.TextField(null=True, blank=True, default='[]', verbose_name='Team members')
+    poster = models.ImageField(upload_to='media', blank=True, null=True, default='')
+    faculty = models.TextField(null=True, blank=True, default='[]', verbose_name='Faculty members')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Events" 
+
+class Registraion(models.Model):
+    event = models.ForeignKey(Events, on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(max_length=100, blank=False, null=False, verbose_name="Student Name")
+    year = models.CharField(max_length=10, blank=False, null=False)
+    branch = models.CharField(max_length=50, blank=False, null=False)
+    scholar_no = models.CharField(max_length=10, blank=True, null=True)
+    enrollment_no = models.CharField(max_length=20, blank=True, null=True)
+    contact_no = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "All Registrations"
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False, verbose_name="Member Name")
+    scholar_no = models.CharField(max_length=10, blank=True, null=True)
+    enrollment_no = models.CharField(max_length=20, blank=True, null=True)
+    contact_no = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField()
+
+    def __str__(self):
+            return self.name
+
+    class Meta:
+        verbose_name_plural = "Team Members"    
+
+class Gallery(models.Model):
+    heading = models.CharField(max_length=50, blank=False, null=False, verbose_name="Image heading")   
+    description = models.CharField(max_length=100, blank=False, null=False, verbose_name="Image description")
+    image = models.ImageField(upload_to='media', blank=False, null=False, default='')      
--- flux_sati/flux/views.py
+++ flux_sati/flux/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    compleate_projects = Projects.objects.filter(completed=True)
+    ongoing_projects = Projects.objects.filter(completed=False)
+    
+    # projects = 
+
+    print(compleate_projects)
+
+    return render(request, 'index.html', {'compleate_projects' : compleate_projects, 'ongoing_projects' : ongoing_projects})
