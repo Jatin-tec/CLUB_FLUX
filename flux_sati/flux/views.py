53b3536d86cc2cd598d6bf99423822ca54d1872c
Jatin-tec <jatin.kshatriya2821@gmail.com>
2021-12-24 11:00:43 +0530

    minor

--- flux_sati/flux/views.py
+++ flux_sati/flux/views.py
@@ -16,27 +16,43 @@ def project_desc(request, id=""):
     return render(request, 'project-details.html', {'project' : project})    
 
 def registration_form(request):
-
+    message = None
+    title= None
+    event = Events.objects.filter()[0]
     if request.method == 'POST':
-        name = request.POST.get('name')
-        year = request.POST.get('year')
-        branch = request.POST.get('branch')
-        email = request.POST.get('email')
-        scholar_number = request.POST.get('scholar_number')
-        enrollment_number = request.POST.get('enrollment_number')
-        phone_number = request.POST.get('phone_number')
+        try:
+            name = request.POST.get('name')
+            year = request.POST.get('year')
+            branch = request.POST.get('branch')
+            email = request.POST.get('email')
+            scholar_number = request.POST.get('scholar_number')
+            enrollment_number = request.POST.get('enrollment_number')
+            phone_number = request.POST.get('phone_number')
+    
+            student = Registraion()
+            student.event = event
+            student.name = name
+            student.year = year
+            student.branch = branch
+            student.email = email
+            student.scholar_no = scholar_number
+            student.enrollment_no = enrollment_number
+            student.contact_no = phone_number
 
-        event = Events.objects.filter()[0]
-        student = Registraion()
-        student.event = event
-        student.name = name
-        student.year = year
-        student.branch = branch
-        student.email = email
-        student.scholar_no = scholar_number
-        student.enrollment_no = enrollment_number
-        student.contact_no = phone_number
+            student.save()        
+            
+            title="primary"
+            message="You registered sucessfully!"
 
-        student.save()        
+        except:
 
-    return render(request, 'event_registration_form.html')    
+            title="danger"
+            message="Something went wrong :("
+
+    return render(request, 'event_registration_form.html', {'title':title, 'message':message, 'event':event})    
+
+
+def subscription(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')    
+        return HttpResponse()
