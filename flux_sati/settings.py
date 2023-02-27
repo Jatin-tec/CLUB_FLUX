2e4137bccbb6d303ec875992b7249d8cc0853e12
Jatin <jatin24ai024@satiengg.in>
2023-02-28 03:48:38 +0530

    removing changes

--- flux_sati/settings.py
+++ flux_sati/settings.py
@@ -147,6 +147,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #Managing Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
