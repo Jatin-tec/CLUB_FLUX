d07faef553cf9ad0627293f66bc0eaa314d4e136
Jatin <jatin24ai024@satiengg.in>
2023-02-27 17:12:15 +0530

    static files

--- flux_sati/settings.py
+++ flux_sati/settings.py
@@ -142,8 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-files")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static-files"),]
 
 #Managing Media
 MEDIA_URL = '/media/'
