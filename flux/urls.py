dc8416965308d153cb18e5d397f5ac4ad9210dcc
Jatin <jatin24ai024@satiengg.in>
2023-02-27 20:23:54 +0530

    urls

--- flux/urls.py
+++ flux/urls.py
@@ -12,3 +12,5 @@ urlpatterns = [
     path('subscribe/', flux_views.subscription, name='subscribe')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
