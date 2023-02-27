b804164f5239f7033fdb8ebf89d18c71fcba6f05
Jatin <jatin24ai024@satiengg.in>
2023-02-27 15:27:35 +0530

    database setup

--- flux_sati/settings.py
+++ flux_sati/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'flux_sati.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'djangodb',
+        'USER': 'djangouser',
+        'PASSWORD': 'fluxsati123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 300, 
     }
 }
 
