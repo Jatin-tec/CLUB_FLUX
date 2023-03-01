12d1dacb0fea7678864b5158324dead0c156674f
Jatin <jatin24ai024@satiengg.in>
2023-03-01 15:30:35 +0530

    dev

--- flux_sati/settings.py
+++ flux_sati/settings.py
@@ -77,25 +77,24 @@ WSGI_APPLICATION = 'flux_sati.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':  'djangodb',
-#         'USER': 'djangouser',
-#         'PASSWORD': 'fluxsati123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#         'CONN_MAX_AGE': 300, 
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'djangodb',
+        'USER': 'djangouser',
+        'PASSWORD': 'fluxsati123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 300, 
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
